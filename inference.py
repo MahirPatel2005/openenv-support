5592e0a2a9ffe776fb197ef581eebdedffcf6f2a
Mahir <mahir.patel.cg@gmail.com>
2026-04-08 20:55:17 +0530

    Checklist Phase II --- part 2

--- inference.py
+++ inference.py
@@ -14,7 +14,7 @@ from openai import OpenAI
 
 API_BASE_URL = os.getenv("API_BASE_URL", "https://api.groq.com/openai/v1")
 MODEL_NAME = os.getenv("MODEL_NAME", "llama-3.3-70b-versatile")
-HF_TOKEN = os.getenv("HF_TOKEN")
+HF_TOKEN = os.getenv("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")
 
 # Optional — if you use from_docker_image():
 LOCAL_IMAGE_NAME = os.getenv("LOCAL_IMAGE_NAME")
